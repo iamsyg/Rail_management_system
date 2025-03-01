569cb1237f5a8a7326343fa03c05d1d18d7170ac
Swayam Gupta <swayamg741@gmail.com>
2025-03-01 19:53:21 +0530

    login-signup-page-layout

--- website/user/app.py
+++ website/user/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def login():
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
