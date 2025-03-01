224693af59bd6dbef2d7c76c53321137d6084173
Swayam Gupta <swayamg741@gmail.com>
2025-03-01 21:21:00 +0530

    SQLalchemyDB-setup

--- website/user/app.py
+++ website/user/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_prefixed_env()
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+db = SQLAlchemy(app)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def login():
