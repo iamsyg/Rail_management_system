b9a9a091e5c99124947ea9b0dce53eace03765dc
Swayam Gupta <swayamg741@gmail.com>
2025-03-04 07:44:38 +0530

    jwt-verification

--- website/user/app.py
+++ website/user/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, render_template, session, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from models import db, jwt
 from auth import auth_bp
@@ -28,6 +28,34 @@ def status():
     if "name" not in session: 
         return redirect("/auth/login")  
     return render_template("status.html")
+
+
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_data):
+    return jsonify({"message": "Token has expired", "error": "token_expired"}), 401
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return (
+        jsonify(
+            {"message": "Signature verification failed", "error": "invalid_token"}
+        ),
+        401,
+    )
+
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return (
+        jsonify(
+            {
+                "message": "Request doesnt contain valid token",
+                "error": "authorization_header",
+            }
+        ),
+        401,
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
